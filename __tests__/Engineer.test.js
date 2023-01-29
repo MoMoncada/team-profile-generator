9dbffc6b71167d2040e0ea827b05bf85d60b9699
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-01-29 23:57:38 +0800

    changed minor typo in Engineer.test script, added template for html page and css file

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,12 +2,12 @@
 const Engineer = require('../lib/Engineer');
 
 //--- passing specic parameters for testing ---//
-const engineer = new Engineer('Hermione', '62442','queen@ministryofmagic.com','hGranger');
+const engineer = new Engineer('Hermione Granger', '62442','queen@ministryofmagic.com','hGranger');
 
 
 //--- first test verifies the values passed to the object constructor and are assigned to the right properties ---//
 test('test if the values passed to the engineer object constructor are being correctly assign to the properties', () => {
-    expect(engineer.name).toBe('Hermione');
+    expect(engineer.name).toBe('Hermione Granger');
     expect(engineer.id).toBe('62442');
     expect(engineer.email).toBe('queen@ministryofmagic.com');
     expect(engineer.githubUserName).toBe('hGranger');
@@ -16,7 +16,7 @@ test('test if the values passed to the engineer object constructor are being cor
 
 //--- the remaining tests verify if the methods return the expected values ---//
 test('test if we can get the name from the getName() method', () => {
-    expect(engineer.getName()).toBe('Hermione');
+    expect(engineer.getName()).toBe('Hermione Granger');
 });
 
 test('test if we can get the id from the getId() method', () => {
